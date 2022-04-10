c2b49fa56bc287c36bb6c969a2762146262c7282
ShreyWadh <103119859+ShreyWadh@users.noreply.github.com>
2022-04-10 12:56:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-alert("Welcome to Stock Market");
+//Just an alert
+alert("Welcome to Stock Market❤❤");
+//Asking a user to enter the name and storing the username in variable named 'name'
+var name=prompt("What is your Name?");
+//pick the first character of the name
+var firstcharacter=name.slice(0,1);
+//Capitalize the first character
+var capfirst=firstcharacter.toUpperCase();
+
+//pick the rest characters from the variable name
+var restofcharacters=name.slice(1,name.length);
+var lowercase=restofcharacters.toLowerCase();
+var finalname=capfirst + lowercase;
+
+
+alert("Hello, " + finalname);
